2e52b3442364438bc60e5625bc6b6a03d16c1ce8
lcsrbn <luca.sorbini@gmail.com>
2020-05-18 17:32:07 +0200

    added video to location page

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,16 +5,16 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-templates-shows-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/templates/shows.js"))),
-  "component---src-templates-news-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/templates/news.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/pages/404.js"))),
-  "component---src-pages-contatti-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/pages/contatti.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/pages/index.js"))),
-  "component---src-pages-info-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/pages/info.js"))),
-  "component---src-pages-lineupandtickets-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/pages/lineupandtickets.js"))),
-  "component---src-pages-location-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/pages/location.js"))),
-  "component---src-pages-news-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/pages/news.js"))),
-  "component---src-pages-privacycookyepolicy-js": hot(preferDefault(require("/Users/k/Desktop/UPM Milano/src/pages/privacycookyepolicy.js")))
+  "component---src-templates-shows-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/templates/shows.js"))),
+  "component---src-templates-news-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/templates/news.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/pages/404.js"))),
+  "component---src-pages-contatti-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/pages/contatti.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/pages/index.js"))),
+  "component---src-pages-info-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/pages/info.js"))),
+  "component---src-pages-lineupandtickets-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/pages/lineupandtickets.js"))),
+  "component---src-pages-location-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/pages/location.js"))),
+  "component---src-pages-news-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/pages/news.js"))),
+  "component---src-pages-privacycookyepolicy-js": hot(preferDefault(require("/Users/k/Desktop/upmilano/src/pages/privacycookyepolicy.js")))
 }
 
--- src/pages/location.js
+++ src/pages/location.js
@@ -9,11 +9,24 @@ import locationimg3 from "../images/location/location-3.png"
 import locationimg4 from "../images/location/location-4.png"
 import locationimg5 from "../images/location/location-5.png"
 import locationimg6 from "../images/location/location-6.png"
+import locationvideo from "../images/location/UPM.mp4"
 
 const Location = () => {
   return (
   <Layout>
     <SEO title="Location"  description="Immagini del Parco e della struttura"/>
+    
+    <div>
+      <iframe className={locationStyle.video}
+        src={locationvideo}
+        title="UPM video"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        frameBorder="0"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        allowFullScreen
+      />
+    </div>
 
     <Carousel className={locationStyle.carousel} className="d-none d-sm-block">
       <Carousel.Item className={locationStyle.carouselitem}>
